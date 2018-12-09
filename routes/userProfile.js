9ea84f60b69b3be28035199a59fc5874eb08bd74
Teresa Romero <tromerolozano@gmail.com>
2018-12-09 12:01:00 +0100

    user profile view and edit information

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const {isLoggedOut, isLoggedIn} = require('../middlewares/isLogged');
 
+const User = require("../models/User");
+
+
 /* GET home page */
 router.get('/profile', isLoggedIn('/auth/login'), (req, res, next) => {
-  res.render('userProfile/index', {actual_page: 'userProfile_page'});
+  res.render('userProfile', {actual_page: 'userProfile_page'});
 });
+
+router.get('/profile/edit', isLoggedIn('/auth/login'), (req, res, next) => {
+  res.render('userProfile/edit', {actual_page: 'editUserProfile_page'});
+});
+
+router.post('/profile/edit', (req,res) => {
+  const {username, email,userId} = req.body;
+  User.findByIdAndUpdate(userId,{username, email})
+     .then(() =>  res.redirect(`/profile`))
+})
 
 
 module.exports = router;
