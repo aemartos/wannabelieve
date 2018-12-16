fdfd449d85207dcf604c80e3809bab3beabab305
aemartos <aemartos@gmail.com>
2018-12-17 00:13:18 +0100

    Delete key from routes seeds

--- bin/seedsRoutes.js
+++ bin/seedsRoutes.js
@@ -14,28 +14,28 @@ mongoose.connect(process.env.DBURL, {useNewUrlParser: true})
 let routes = [
   {
     routetitle: 'UFO sightings',
-    description: 'If you have never seen a UFO and want too... then watch seven sunsets in a row and I promise you will see something that cannot be explained away.',
-    phenomsId: [{_id: "5c14bfbadde3760898eda62b"}, {_id: "5c14bfbadde3760898eda62c"}, {_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}]
+    //phenomsId: [{_id: "5c14bfbadde3760898eda62b"}, {_id: "5c14bfbadde3760898eda62c"}, {_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}],
+    description: 'If you have never seen a UFO and want too... then watch seven sunsets in a row and I promise you will see something that cannot be explained away.'
   },
   {
     routetitle: "Unicorn's path",
-    description: 'Al ataquerl apetecan ese que llega quietooor fistro no puedor por la gloria de mi madre de la pradera a wan.',
-    phenomsId: [{_id: "5c14bfbadde3760898eda62b"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda630"}]
+    // phenomsId: [{_id: "5c14bfbadde3760898eda62b"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda630"}],
+    description: 'Al ataquerl apetecan ese que llega quietooor fistro no puedor por la gloria de mi madre de la pradera a wan.'
   },
   {
     routetitle: 'Natural way',
-    description: 'Lorem fistrum pupita me cago en tus muelas qué dise usteer a gramenawer llevame al sircoo está la cosa muy malar al ataquerl a peich.',
-    phenomsId: [{_id: "5c14bfbadde3760898eda62c"}, {_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}]
+    //phenomsId: [{_id: "5c14bfbadde3760898eda62c"}, {_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}],
+    description: 'Lorem fistrum pupita me cago en tus muelas qué dise usteer a gramenawer llevame al sircoo está la cosa muy malar al ataquerl a peich.'
   },
   {
     routetitle: 'Flipping in colors',
-    description: 'Qué dise usteer me cago en tus muelas ese pedazo de por la gloria de mi madre va usté muy cargadoo. ',
-    phenomsId: [{_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}]
+    //phenomsId: [{_id: "5c14bfbadde3760898eda62d"}, {_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}],
+    description: 'Qué dise usteer me cago en tus muelas ese pedazo de por la gloria de mi madre va usté muy cargadoo. '
   },
   {
     routetitle: 'Mysterious route',
-    description: 'A wan ese pedazo de me cago en tus muelas ese hombree a peich no puedor.',
-    phenomsId: [{_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}]
+    //phenomsId: [{_id: "5c14bfbadde3760898eda62e"}, {_id: "5c14bfbadde3760898eda62f"}, {_id: "5c14bfbadde3760898eda630"}],
+    description: 'A wan ese pedazo de me cago en tus muelas ese hombree a peich no puedor.'
   }
 ];
 
